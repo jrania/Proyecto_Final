ab356dfe0870b710d0db9a6c83b240aa54a9c1c2
jrania <65193014+jrania@users.noreply.github.com>
2023-07-17 10:54:00 -0300

    Create regresion.h

---
+++ src/regresion/regresion.h
@@ -0,0 +1,4 @@
+void constantesRegresion(float *, float *, int *, int, unsigned long *, int);
+float terminoConstante();
+float terminoOrdenada(float);
+void calculoSumas(int *, int, unsigned long *, int);
